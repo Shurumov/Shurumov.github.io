11180b65934a7b294bc94dd898a009504f0da81f
Shurumov <alexei0113@gmail.com>
2020-03-09 22:59:57 +0700

    add two apps

---
+++ news-redux/precache-manifest.5d2ff3387737bc605c8dc55911d02e0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0491953947393dc81b8ce3d296e2d4ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42744fc229a43f3bcd3f",
+    "url": "/static/css/2.03d7f783.chunk.css"
+  },
+  {
+    "revision": "5e30f3fbf1928adb9e50",
+    "url": "/static/css/main.f6b6d8fa.chunk.css"
+  },
+  {
+    "revision": "42744fc229a43f3bcd3f",
+    "url": "/static/js/2.c6e079d5.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.c6e079d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e30f3fbf1928adb9e50",
+    "url": "/static/js/main.8ccee729.chunk.js"
+  },
+  {
+    "revision": "712e5f8aac36538ef77d",
+    "url": "/static/js/runtime-main.50f09b17.js"
+  }
+]);
